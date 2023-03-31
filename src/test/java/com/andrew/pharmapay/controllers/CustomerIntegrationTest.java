7bbbafb4a244f41e7b94e289281764d9b7119a5c
Andrew Owino <andystudy4@gmail.com>
2023-04-01 00:41:34 +0300

    Test creation of customer record with valid inputs

---
+++ src/test/java/com/andrew/pharmapay/controllers/CustomerIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.andrew.pharmapay.controllers;
+
+import com.andrew.pharmapay.models.Customer;
+import com.andrew.pharmapay.models.Pharmacist;
+import com.andrew.pharmapay.repositories.CustomerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CustomerIntegrationTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @AfterEach
+    void tearDown() {
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    public void createPharmacist_withValidData_shouldReturnCreated() throws Exception {
+        Customer customer = new Customer("John", "Doe");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/customers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer)))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+
+        List<Customer> allCustomers = customerRepository.findAll();
+        assertEquals(1, allCustomers.size());
+        assertEquals("John", allCustomers.get(0).getFirstName());
+    }
+}
