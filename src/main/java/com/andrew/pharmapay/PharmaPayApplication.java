824fae3d15782ab853f515b275ecc007b48b46d2
Andrew Owino <andystudy4@gmail.com>
2023-03-31 13:36:31 +0300

    Initial commit

---
+++ src/main/java/com/andrew/pharmapay/PharmaPayApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.pharmapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaPayApplication.class, args);
+	}
+
+}
