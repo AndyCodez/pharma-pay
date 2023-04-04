fbed6b17a56e1cd784e1de2a8d2abc4c8a3378f5
Andrew Owino <andystudy4@gmail.com>
2023-04-04 15:04:47 +0300

    Add exception thrown to method signature

--- src/main/java/com/andrew/pharmapay/controllers/BillController.java
+++ src/main/java/com/andrew/pharmapay/controllers/BillController.java
@@ -1,6 +1,7 @@
 package com.andrew.pharmapay.controllers;
 
 import com.andrew.pharmapay.exceptions.ItemNotInStockException;
+import com.andrew.pharmapay.exceptions.LessItemInStockException;
 import com.andrew.pharmapay.models.Bill;
 import com.andrew.pharmapay.models.SoldItem;
 import com.andrew.pharmapay.payloads.BillResponse;
@@ -22,7 +23,7 @@ public class BillController {
     }
 
     @PostMapping("/bills")
-    public ResponseEntity<Bill> createNewBill(@RequestBody List<SoldItem> soldItems) throws ItemNotInStockException {
+    public ResponseEntity<Bill> createNewBill(@RequestBody List<SoldItem> soldItems) throws ItemNotInStockException, LessItemInStockException {
         Bill createdBill = billService.createBill(soldItems);
         return ResponseEntity.created(
                 URI.create("/api/v1/bills" + createdBill.getId())
