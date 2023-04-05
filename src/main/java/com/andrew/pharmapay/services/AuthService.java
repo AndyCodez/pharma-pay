866e7d7f4a4f186949b51108eeb18c24c54b308d
Andrew Owino <andystudy4@gmail.com>
2023-04-05 23:31:16 +0300

    Implement sign-in logic

---
+++ src/main/java/com/andrew/pharmapay/services/AuthService.java
@@ -0,0 +1,39 @@
+package com.andrew.pharmapay.services;
+
+import com.andrew.pharmapay.payloads.AuthRequest;
+import com.andrew.pharmapay.payloads.AuthResponse;
+import com.andrew.pharmapay.repositories.PharmacistRepository;
+import com.andrew.pharmapay.utils.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final PharmacistRepository pharmacistRepository;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(AuthenticationManager authenticationManager, PharmacistRepository pharmacistRepository, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.pharmacistRepository = pharmacistRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public AuthResponse authenticateUser(AuthRequest request) throws Exception {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+        try {
+            authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect Username or Password", e);
+        }
+
+        var user =  this.pharmacistRepository.findByEmail(request.getEmail()).orElseThrow();
+
+        String jwtToken = jwtUtil.generateToken(user);
+
+        return new AuthResponse(jwtToken);
+    }
+}
