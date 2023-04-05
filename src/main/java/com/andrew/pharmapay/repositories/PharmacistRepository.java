2f1cca76bdbaaa181185b634815c4f5e4998c394
Andrew Owino <andystudy4@gmail.com>
2023-04-05 23:32:12 +0300

    Add a method to find pharmacist by email (used in the sign-in logic)

--- src/main/java/com/andrew/pharmapay/repositories/PharmacistRepository.java
+++ src/main/java/com/andrew/pharmapay/repositories/PharmacistRepository.java
@@ -2,8 +2,12 @@ package com.andrew.pharmapay.repositories;
 
 import com.andrew.pharmapay.models.Pharmacist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PharmacistRepository extends JpaRepository<Pharmacist, Long> {
+    Optional<UserDetails> findByEmail(String email);
 }
